86fc03565e48ef5219cb6ac865eccd1690a933d6
SiYeungLee@TIP209 <LeeSiYeoung.h4801y@naver.com>
2022-11-23 09:47:00 +0900

    Update move_character_with_mouse.py

--- Labs/Lecture06_HandlingInputs/move_character_with_mouse.py
+++ Labs/Lecture06_HandlingInputs/move_character_with_mouse.py
@@ -6,8 +6,14 @@ def handle_events():
     # fill here
     pass
 
+<<<<<<< HEAD
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
+=======
+
+# fill here
+kpu_ground = load_image('TUK_GROUND.png')
+>>>>>>> cbd8c89261a2af2e3106815348b0bb39a1d37b9a
 character = load_image('animation_sheet.png')
 
 running = True
